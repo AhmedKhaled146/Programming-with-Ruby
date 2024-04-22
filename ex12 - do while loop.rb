ca6f348c1d732ebf676698416c4a87550a92bab7
AhmedKhaled146 <a7med74yaso@gmail.com>
2024-04-22 14:31:27 +0200

    add examples for the looping

---
+++ ex12 - do while loop.rb
@@ -0,0 +1,29 @@
+# Algorithm
+# 1. Start
+# 2. Output the prompt "Calculate the avg of 6 students grade."
+# 3. Initialize a variable counter to 0
+# 4. Initialize a variable sum to 0
+# 5. Enter a loop:
+#    6. Prompt the user to input the grade of student counter + 1
+#    7. Read the input grade (grade)
+#    8. Convert the input grade to an integer
+#    9. Add the input grade to the sum
+#   10. Increment the counter by 1
+#   11. If counter is equal to 6:
+#         12. Exit the loop
+# 12. Output the average of the 6 student grades by dividing the sum by 6
+# 13. End
+
+puts 'Calculate the avg of 6 students grade.'
+counter = 0
+sum = 0
+loop do
+  print "Enter the grade of student #{counter + 1}: "
+  grade = gets.chomp.to_i
+  sum += grade
+  counter += 1
+  if counter == 6
+    break
+  end
+end
+puts "The AVG of 6 student grades is #{sum/6}"
---
+++ ex13 - times loop.rb
@@ -0,0 +1,25 @@
+# Algorithm
+# 1. Start
+# 2. Output the prompt "Calculate the avg of 6 students grade."
+# 3. Initialize a variable sum to 0
+# 4. Use a loop to iterate exactly 6 times:
+#      5. For each iteration, prompt the user to input the grade of the current student
+#      6. Read the input grade (grade)
+#      7. Convert the input grade to an integer
+#      8. Add the input grade to the sum
+# 9. Calculate the average by dividing the sum by 6
+# 10. Output the average of the 6 student grades
+# 11. End
+
+
+puts "Calculate the avg of 6 students grade."
+sum = 0
+
+6.times do |counter|
+  print "Enter the grade of student #{counter + 1}: "
+  grade = gets.chomp.to_i
+  sum += grade
+end
+
+average = sum.to_f / 6
+puts "The average of 6 student grades is #{average}"
